400c42dbd4b14e64e776de61a2df12ce9eeafe6b
noobosaurus-r3x <97100614+noobosaurus-r3x@users.noreply.github.com>
2023-03-15 13:02:45 +0100

    Update crawwwler.sh

--- crawwwler.sh
+++ crawwwler.sh
@@ -25,13 +25,54 @@ echo "$IMAGE"
 url="$1"
 
 # Download the HTML page from the website
-response=$(curl -sL "$url")
+response=$(curl -sL --fail "$url")
 
 # Check if curl was successful
 if [ $? -ne 0 ]; then
   echo "Error: failed to download HTML page"
   exit 1
 fi
+
+# Extract all the links from the HTML page
+links=$(echo "$response" | grep -oP '(?<=<a href=")[^"]*')
+
+# Define a function to check the HTTP status code of a URL
+check_status() {
+  local url=$1
+
+  # Get the HTTP status code of the URL
+  local code=$(curl -sL -o /dev/null -w "%{http_code}" "$url")
+
+  # Print the URL and its HTTP status code, truncating the URL to 100 characters
+  if [ $code -eq 200 ]; then
+    # Print the URL in green if the status code is 200
+    printf "%-100s \e[32m%s\e[0m\n" "${url:0:100}" "$code"
+  else
+    # Print the URL in red if the status code is not 200
+    printf "%-100s \e[31m%s\e[0m\n" "${url:0:100}" "$code"
+  fi
+}
+
+# Loop through each link and check its HTTP status code
+for link in $links; do
+
+  # Check if the link is a relative or absolute URL
+  if [[ $link =~ ^http[s]?:// ]]; then
+    # The link is an absolute URL
+    url=$link
+  else
+    # The link is a relative URL
+    url="$url/$link"
+  fi
+
+  check_status "$url"
+done | tee "results_$(basename $1).txt"
+
+# Print the file path of the output file
+echo "--------------------------------------------------"
+echo "You can find the results here: $(readlink -f "results_$(basename $1).txt")"
+echo "--------------------------------------------------"
+
 
 # Extract all the links from the HTML page
 links=$(echo "$response" | grep -oP '(?<=<a href=")[^"]*')
